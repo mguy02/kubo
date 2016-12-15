906b7460a370a6b56feea0171536955e1e6264e7
weilenmc <chriswei@hispeed.ch>
2016-12-15 16:22:25 +0000

    Updated KuBo_test for only listening

--- KuBo_test.py
+++ KuBo_test.py
@@ -5,16 +5,40 @@ import time
 from KuBo import KuBo
 
 kubo = KuBo()
-print "Kubo is jumping: ", kubo.is_jumping()
-kubo.start_jumping(1500, 1.06)
 
-time.sleep(10)
-print "Kubo is jumping: ", kubo.is_jumping()
-kubo.change_jumping(2100, 0.53)
-
-time.sleep(10)
-print "Kubo is jumping: ", kubo.is_jumping()	
-kubo.stop_jumping()
-
+######################### Jumping and Sound Tests ##############################
+"""
+print "Kubo says KuBo and jumps"
+kubo.say('kuuu.mp3')
 time.sleep(1)
-print "Kubo is jumping: ", kubo.is_jumping()
+
+kubo.say('bo.mp3')
+# Frequency = 0 means kubo jumps only once and exits the thread automatically
+# No stop_jumping() needed
+kubo.start_jumping(1500, 0)
+time.sleep(2)
+
+
+print "Kubo is more dramatic"
+kubo.say('kuuu.mp3')
+time.sleep(3)
+
+kubo.say('bo.mp3')
+kubo.start_jumping(2000, 0)
+time.sleep(2)
+
+# Kill potential open omx-processes
+kubo.stop_voice()
+"""
+######################### Interrupt Tests #################################
+print "Listening for interrupts..."
+
+while True:
+	try:		
+		time.sleep(2)
+	except KeyboardInterrupt:
+		kubo.stop_voice()
+		kubo.stop_listening()
+		raise
+
+
