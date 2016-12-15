19a0b11c6efaa5c562a2870ea584b8d5a890b67e
mguy02 <zelordking@gmail.com>
2016-12-15 16:32:30 +0100

    More control on getting the test

--- reed_test.py
+++ reed_test.py
@@ -4,6 +4,9 @@ import RPi.GPIO as GPIO
 import numpy as np
 import time
 import sys
+import csv
+
+
 GPIO.setmode(GPIO.BCM)
 
 # Sensor pins
@@ -18,6 +21,9 @@ arr2 = []
 
 print "Listening to Reed contacts..."
 
+sample_period = 0.001
+close = False	#if magnets are close or not
+
 while True:
 	val_1 = GPIO.input(pin_s1)
 	val_2 = GPIO.input(pin_s2)
@@ -26,19 +32,19 @@ while True:
 
 	arr2.append(val_2)
 	
-	time.sleep(0.001)
+	time.sleep(sample_period)
 	idx_1 = idx_1 + 1
 	if idx_1 > 10000:
 		break
 
-sys.stdout.write ("[")
-for e in arr1:
-	if e == 0:
-		sys.stdout.write (".")
-	else:
-		sys.stdout.write (str(e))
-	sys.stdout.write (",")
-print "]"
+#sys.stdout.write ("[")
+#for e in arr1:
+#	if e == 0:
+#		sys.stdout.write (".")
+#	else:
+#		sys.stdout.write (str(e))
+#	sys.stdout.write (",")
+#print "]"
 
 
 sys.stdout.write ("[")
@@ -49,10 +55,32 @@ for e in arr2:
 		sys.stdout.write (str(e))
 	sys.stdout.write (",")
 print "]"
-	
-with open("rawdata.txt", "w") as f:
-	f.write(str(arr1))
-	f.write("\n")
-	f.write(str(arr2))
 
 
+
+#asking for number of up and down in a safe way (wrong value will ask again)
+nb_ud = -1
+while nb_ud < 0:
+	nb_ud = input("Number of ud:")
+	try:
+		nb_ud = int(nb_ud)
+	except ValueError:
+		nb_ud = -1
+		continue
+
+#now define the filename
+fname = "ud"*nb_ud
+fname += "_"+str(sample_period)
+
+if close:
+	fname += "_close"
+
+fname += ".csv"
+
+#Output as csv file
+with open(fname, 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter='\t')
+    for val in arr2:
+		writer.writerow([val])
+
+print "Saved to", fname
