cd8b1900ab00bda659a9dbba61db15adac71cd1a
weilenmc <chriswei@hispeed.ch>
2016-12-21 13:08:23 +0000

    -Improved accuracy of error detection -Calculation of weight, speed -Setting of flag when last magnet of block is detected

--- KuBo.py
+++ KuBo.py
@@ -11,12 +11,15 @@ import numpy as np
 class KuBo():
 	
 	
-	def __init__(self, servoPin = 19, lowerReedPin = 17, higherReedPin = 22, start_pos = 900):
+	def __init__(self, servoPin = 19, lowerReedPin = 17, higherReedPin = 22, start_pos = 900, plate_dist = 0.02, plate_weight = 5):
 		self.servoPin = servoPin
 		self.lowerReedPin = lowerReedPin
 		self.higherReedPin = higherReedPin
 			
 		self.start_pos = start_pos	#The servo position at boot up
+		
+		self.plate_dist = plate_dist
+		self.plate_weight = plate_weight
 		
 		self.pi = pigpio.pi()
 		
@@ -24,6 +27,7 @@ class KuBo():
 		self.pi.set_mode(self.servoPin, pigpio.OUTPUT)
 		self.pi.set_mode(self.lowerReedPin, pigpio.INPUT)
 		self.pi.set_mode(self.higherReedPin, pigpio.INPUT)
+		
 		
 		# Set position of servo to start position
 		self.pi.set_servo_pulsewidth(self.servoPin, self.start_pos)
@@ -41,22 +45,37 @@ class KuBo():
 		# Initiallize edge listener
 		self.cb_lower = self.pi.callback(self.lowerReedPin, pigpio.RISING_EDGE, self._callback_lower)
 		self.cb_higher = self.pi.callback(self.higherReedPin, pigpio.RISING_EDGE, self._callback_higher)
+				
+		# zero count parameters
+		self.max_zero_count = 45
+		# self.max_zero_count = 1000
+		self.min_zero_count = 5
 		
-		self.last_cb_lower = 0
-		self.last_cb_higher = 0
+		# one count parameters
+		self.min_one_count = 3
 		
-		# Debounce parameters
-		self.deb = 0.12
-		self.num_it = 5
-		self.it_delay = 0.005
+		# polling period
+		self.T_poll = 0.001
 		
-		# Contact counter
-		self.lower_count = 0
-		self.higher_count = 0
+		# lower reed contact data
+		self.lower_speed = 0
+		self.lower_weight = 0
+		self.lower_flag = 0
+		self.lower_timestamp = 0
+		
+		
+		# higher reed contact data
+		self.higher_speed = 0
+		self.higher_weight = 0
+		self.higher_flag = 0
+		self.higher_timestamp = 0
+		
 		
 		# Locks
-		self.lock_cb_lower = threading.Lock()
-		self.lock_cb_higher = threading.Lock()
+		self.lock_lower = threading.Lock()
+		self.lock_higher = threading.Lock()
+		
+		self.data = []
 
 		
 	def start_jumping(self, end_pos, freq):
@@ -158,75 +177,203 @@ class KuBo():
 		return self.omx.quit()
 		
 	def _callback_lower(self, gpio, level, tick):
+		
 		t = time.time()
 		
-		# debounce
-		with self.lock_cb_lower:
-			t_diff = t - self.last_cb_lower
-		if  t_diff < self.deb:
-			# caught debounce
-			return
-		else:
-			# despike
-			sum = 0
-			it_count = 0
-			while it_count < self.num_it:
-				sum = sum + self.pi.read(self.lowerReedPin)
-				it_count = it_count + 1
-				time.sleep(self.it_delay)
-			if sum/self.num_it > 0.5:
-				# legitamite contact
-				print "====Lower Reed contact activated===="
-				print "System Time at interrupt: ", t
-				print "Pin: ", gpio
-				print "Level: ", level
-				
-				with self.lock_cb_lower:
-					self.last_cb_lower = t
-					self.lower_count = self.lower_count + 1
-				print "Lower Reed contact count: ", self.lower_count
-				print
-			else:
-				# interference
-				print "Interference detected on lower Reed Contact on Pin: ", self.lowerReedPin 
+		# stop interrupts
+		self.cb_lower.cancel()
 		
+		self.data.append('...')
+		
+		print "==== Callback lower reed contact ===="
+		
+		timestamps = []
+		magnet_count = 0
+		one_count = 0
+		while True:
+			inter_flag = False
+			input = 1
+			while input:
+				input = self.pi.read(gpio)
+				
+				# debug
+				# self.data.append(input)
+				
+				one_count = one_count + 1
+				time.sleep(self.T_poll)
+			if one_count < self.min_one_count:
+				# Interference if no valey interference compensates for it
+				# print "Peak Interference suspicion"
+				inter_flag = True
+			
+			
+			zero_count = 0
+			input = 0
+			while input == 0:
+				input = self.pi.read(gpio)
+				
+				# debug
+				# self.data.append(input)
+				
+				zero_count = zero_count + 1
+				time.sleep(self.T_poll)
+				if zero_count > self.max_zero_count:
+					# No more consecutive magnets --> activate 
+					# interrupts again for next passing of weights
+					break
+				elif inter_flag and zero_count >= self.min_zero_count:
+					# No valey interference to compensate for peak interference
+					# check if interference is isolated
+					# print "Peak interference detected on lower contact"
+					if magnet_count == 0:
+						# isolated interference
+						self.cb_lower = self.pi.callback(gpio, pigpio.RISING_EDGE, self._callback_lower)
+						print "Interference detected"
+						return
+					else:
+						# interference between magnets --> treet ones as zeros
+						zero_count = zero_count + one_count
+			if zero_count < self.min_zero_count:
+				pass
+				# Interference --> continue with first while loop
+				# print "Valey Interference detected on lower contact"
+				# if inter_flag:
+					# print "No peak interference"
+			elif zero_count > self.max_zero_count:
+				# No more consecutive magnets --> calculate values of one repetition
+				magnet_count = magnet_count + 1
+				print "Magnet Count: ", magnet_count
+				timestamps.append(t) # save old timestamp
+				print " Timestamps: ", timestamps
+				
+				v = 0
+				if magnet_count > 1:
+					for i in range(magnet_count-1):
+						v = v + self.plate_dist / (timestamps[i+1] - timestamps[i])
+				else:
+					print "Only one magnet detected --> cannot determine speed"
+				
+				with self.lock_lower: # save values of one repetition
+					self.lower_weight = self.plate_weight*magnet_count
+					self.lower_speed = v / magnet_count
+					if self.lower_flag:
+						print "flag wasn't read or properly set back to 0"
+					self.lower_flag = 1
+					self.lower_timestamp = timestamps[magnet_count-1]
+					
+					
+				print "End of consecutive magnets"
+				# Reactivate interrupt listener
+				self.cb_lower = self.pi.callback(gpio, pigpio.RISING_EDGE, self._callback_lower)
+				break
+			else:
+				# start of next magnet
+				timestamps.append(t) # save old timestamp
+				t = time.time()
+				one_count = 0
+				magnet_count = magnet_count + 1
+				print "Magnet Count: ", magnet_count
+					
+				
+			
+			
 	def _callback_higher(self, gpio, level, tick):
 		t = time.time()
 		
-		# debounce
-		with self.lock_cb_higher:
-			t_diff = t - self.last_cb_higher
-		if  t_diff < self.deb:
-			# caught debounce
-			return
-		else:
-			# despike
-			sum = 0
-			it_count = 0
-			while it_count < self.num_it:
-				sum = sum + self.pi.read(self.higherReedPin)
-				it_count = it_count + 1
-				time.sleep(self.it_delay)
-			if sum/self.num_it > 0.5:
-				# legitimate contact
-				print "====Higher Reed contact activated===="
-				print "System Time at interrupt: ", t
-				print "Pin: ", gpio
-				print "Level: ", level
-				# time.sleep(1)
-				# print "Tick difference: ", self.pi.get_current_tick() - tick
-				# print "Time difference: ", time.time() - t
-				
-				
-				with self.lock_cb_higher:
-					self.last_cb_higher = t
-					self.higher_count = self.higher_count + 1
-				print "Higher Reed contact count: ", self.higher_count
-				print
-			else:
-				# interference
-				print "Interference detected on higher Reed Contact on Pin: ", self.higherReedPin 
+		# stop interrupts
+		self.cb_higher.cancel()
 		
+		self.data.append('...')
+		
+		print "==== Callback higher reed contact ===="
+		
+		timestamps = []
+		magnet_count = 0
+		one_count = 0
+		while True:
+			inter_flag = False
+			input = 1
+			while input:
+				input = self.pi.read(gpio)
+				
+				# debug
+				# self.data.append(input)
+				
+				one_count = one_count + 1
+				time.sleep(self.T_poll)
+			if one_count < self.min_one_count:
+				# Interference if no valey interference compensates for it
+				# print "Peak Interference suspicion"
+				inter_flag = True
+			
+			
+			zero_count = 0
+			input = 0
+			while input == 0:
+				input = self.pi.read(gpio)
+				
+				# debug
+				# self.data.append(input)
+				
+				zero_count = zero_count + 1
+				time.sleep(self.T_poll)
+				if zero_count > self.max_zero_count:
+					# No more consecutive magnets --> activate 
+					# interrupts again for next passing of weights
+					break
+				elif inter_flag and zero_count >= self.min_zero_count:
+					# No valey interference to compensate for peak interference
+					# check if interference is isolated
+					# print "Peak interference detected on higher contact"
+					if magnet_count == 0:
+						# isolated interference
+						self.cb_higher = self.pi.callback(gpio, pigpio.RISING_EDGE, self._callback_higher)
+						print "Interference detected"
+						return
+					else:
+						# interference between magnets --> treet ones as zeros
+						zero_count = zero_count + one_count
+			if zero_count < self.min_zero_count:
+				pass
+				# Interference --> continue with first while loop
+				# print "Valey Interference detected on higher contact"
+				# if inter_flag:
+					# print "No peak interference"
+			elif zero_count > self.max_zero_count:
+				# No more consecutive magnets --> calculate values of one repetition
+				magnet_count = magnet_count + 1
+				print "Magnet Count: ", magnet_count
+				timestamps.append(t) # save old timestamp
+				print " Timestamps: ", timestamps
+				
+				v = 0
+				if magnet_count > 1:
+					for i in range(magnet_count-1):
+						v = v + self.plate_dist / (timestamps[i+1] - timestamps[i])
+				else:
+					print "Only one magnet detected --> cannot determine speed"
+				
+				with self.lock_higher: # save values of one repetition
+					self.higher_weight = self.plate_weight*magnet_count
+					self.higher_speed = v / magnet_count
+					if self.higher_flag:
+						print "flag wasn't read or properly set back to 0"
+					self.higher_flag = 1
+					self.higher_timestamp = timestamps[magnet_count-1]
+					
+					
+				print "End of consecutive magnets"
+				# Reactivate interrupt listener
+				self.cb_higher = self.pi.callback(gpio, pigpio.RISING_EDGE, self._callback_higher)
+				break
+			else:
+				# start of next magnet
+				timestamps.append(t) # save old timestamp
+				t = time.time()
+				one_count = 0
+				magnet_count = magnet_count + 1
+				print "Magnet Count: ", magnet_count
+			
 	def stop_listening(self):
 		if hasattr(self, 'cb_lower') and hasattr(self, 'cb_higher'):
 			self.cb_lower.cancel()
@@ -241,6 +388,8 @@ class KuBo():
 	def start_listening(self, lowerReedPin = 17, higherReedPin = 22):
 		self.cb_lower = self.pi.callback(self.lowerReedPin, pigpio.RISING_EDGE, self._callback_lower)
 		self.cb_higher = self.pi.callback(self.higherReedPin, pigpio.RISING_EDGE, self._callback_higher)
+		
+		
 			
 
 		
